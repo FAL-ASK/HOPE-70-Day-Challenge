609bdfa0a89cb0a01ef17714f9883c8890167427
Kathirvalavan A S <126230808+FAL-ASK@users.noreply.github.com>
2023-08-01 19:49:03 +0530

    Create appleAndOrange.py

---
+++ appleAndOrange.py
@@ -0,0 +1,59 @@
+"""
+Question : https://www.hackerrank.com/contests/hope-70-days-challenge/challenges/apple-and-orange
+Complete the countApplesAndOranges function in the editor below. It should print the number of apples and oranges that land on Sam's house, each on a separate line.
+
+countApplesAndOranges has the following parameter(s):
+
+s: integer, starting point of Sam's house location.
+t: integer, ending location of Sam's house location.
+a: integer, location of the Apple tree.
+b: integer, location of the Orange tree.
+apples: integer array, distances at which each apple falls from the tree.
+oranges: integer array, distances at which each orange falls from the tree.
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countApplesAndOranges' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER s
+#  2. INTEGER t
+#  3. INTEGER a
+#  4. INTEGER b
+#  5. INTEGER_ARRAY apples
+#  6. INTEGER_ARRAY oranges
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    capp=0
+    corg=0
+    for i in apples:
+        d=i+a
+        if((d>=s) and (d<=t)):
+            capp+=1
+    print(capp)
+    for i in oranges:
+        d=i+b
+        if((d>=s) and (d<=t)):
+            corg+=1
+    print(corg)
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+    s = int(first_multiple_input[0])
+    t = int(first_multiple_input[1])
+    second_multiple_input = input().rstrip().split()
+    a = int(second_multiple_input[0])
+    b = int(second_multiple_input[1])
+    third_multiple_input = input().rstrip().split()
+    m = int(third_multiple_input[0])
+    n = int(third_multiple_input[1])
+    apples = list(map(int, input().rstrip().split()))
+    oranges = list(map(int, input().rstrip().split()))
+    countApplesAndOranges(s, t, a, b, apples, oranges)
